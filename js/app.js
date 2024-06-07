f7300f8a1d3d430755c362d8405a05be4c9a4cdd
Alizah <alizah.berg77@gmail.com>
2024-06-06 22:11:24 -0400

    tictactoerevisions

--- js/app.js
+++ js/app.js
@@ -2,9 +2,22 @@
 /*---------------------------- Variables (state) ----------------------------*/
 
 let winner = false;  
-let tie = false
-let board= document.querySelector('.board');
-let currentTurn = "X"; 
+let tie = false;
+let board;
+let currentTurn; 
+
+
+const winningConditions = [
+    [0, 1, 2],
+    [3, 4, 5], 
+    [6, 7, 8], 
+    [0, 3, 6], 
+    [1, 4, 7], 
+    [2, 5, 8], 
+    [0, 4, 8], 
+    [2, 4, 6]  
+  ];
+
 
 /*------------------------ Cached Element References ------------------------*/
 
@@ -12,109 +25,92 @@ const squareEls = document.querySelectorAll('.sqr');
 const messageEl = document.getElementById('message'); 
 const resetBtnEl = document.getElementById('reset');
 /*-------------------------------- Functions --------------------------------*/
-function init() {
-currentTurn = "X"; 
-winner = false; 
-tie = false; 
-board = 
-    [ ' ',' ',' ',' ',' ',' ',' ',' ',' ']; 
-messageEl.innerText = "Ready to Play"; 
-    render(); 
-}
+const init = () => {
+  currentTurn = 1;  
+  winner = false; 
+  tie = false; 
+  board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']; 
+  messageEl.innerText = "Ready to Play"; 
+  render();
+};
+
 
 const render = () => {
 updateBoard();
 updateMessage(); 
-}
+};
 
 
-const updateBoard = () => 
-    board.forEach(squareEls => {
-        board[index] = squareEls += squareEls.innerText;
-        }); 
-  
-  
+const updateBoard = () => {
+    for (let i = 0; i < squareEls.length; i++) {
+          squareEls[i].innerText = board[i];
+        }
+    };
+        
+      
 const updateMessage = () => {
-    if(tie === false && winner === false) {
-       return; 
-    } else if (tie ===true && winner === false ) {
-        messageEl.innerText = "Tie"
-    } else {
-        messageEl.innerText = "Congrats you won"
-    }
-    }
-     
-
-const handleClick = () => {
-squareEls.forEach(function(squareEl) {
-    squareEl.addEventListener('click', handleClick())
-    })
-
-placePiece(squareIndex); 
-checkForWinner(); 
-checkForTie(); 
-switchPlayerTurn(); 
-render(); 
-}
- 
- 
+        if(winner) {
+          messageEl.innerText = "Congrats you won";
+        } else if (tie) {
+          messageEl.innerText = "Tie";
+        } else {
+          messageEl.innerText = `Player ${currentTurn === 1 ? 'X' : 'O'}'s turn`; 
+        }
+        
+    };
 
 const placePiece = (index) => { 
-board[index] = currentTurn.valueOf(); 
-}
+  board[index] = currentTurn === 1 ? 'X' : '0';
+squareEls[index].textContent= board[index]; 
+         };
 
 
-const checkForWinner= () => {
-if(board[0] != ' ' && board[0]===board[1] && board[1] === board[2]) { 
-  winner = true;   
-} else if 
-    (board[3] != ' ' && board[3] === board[4] && board[4] === board[5]) {
-        winner = true; 
-    }
-else if (board[6] != ' ' && board[6] === board[7] && board[7] === board[8]) {
-    winner = true; 
-} else if (board[0] != ' ' && board[0]=== board[3] && board[3]=== board[6]) {
-    winner = true; 
-} else if (board[1] != ' ' && board[1]=== board[4] && board[4]=== board[7]) {
-    winner = true; 
-} else if (board[2] != ' ' && board[2]=== board[5] && board[5] === board[8]) {
-    winner = true; 
-} else if (board[0] != ' ' && board[0]===board[4] && board[4]===board[8]) {
-    winner = true; 
-} else if (board[2] != ' ' && board[2]=== board[4] && board[4]===board[6]) {
-    winner= true; 
-}
-}
-
+  const handleClick = (event) => { 
+    const index = parseInt(event.target.id, 10); 
+    if(board[index] !== ' ') return; 
+placePiece(index);
+checkForWinner();
+checkForTie();
+switchPlayerTurn();
+render();
+  }; 
 
 const checkForTie = () => {
-checkForWinner(); 
-if(winner = true) {
-    return;
-} else if (board.includes(' ') = true) {
+  if (board.includes(' ')) {
     tie = false; 
-} else {
+  } else if(winner === true) {
+    tie = false; 
+  } else {
     tie = true; 
+  }
+  
+};
 
-}
-}
-
-const switchPlayerTurn = () => {
-if(winner = true) {
-    return;
-} else if(winner = false) {
-    if(currentTurn='X') {
-        currentTurn='O'
-    } else if (currentTurn='O') {
-        currentTurn='X'
+const checkForWinner= () => {
+  for (let condition of winningConditions) {
+    const [a, b, c] = condition; 
+    if(board[a] !== ' ' && board[a] === board[b] && board[b] === board[c]) {
+      winner = true; 
+      return
     }
-}
-}
+  } 
+  winner = false; 
+    };
+    
+const switchPlayerTurn = () => {
+  if(winner) 
+    return; 
+currentTurn = currentTurn === 1 ? -1 : 1;  
+};
 
 
 /*----------------------------- Event Listeners -----------------------------*/
-squareEls.forEach(function(squareEl) {
-    squareEl.addEventListener('click', handleClick())
-    })
 
-resetBtnEl.addEventListener('click', init()); 
+squareEls.forEach((square, index) => { 
+    square.addEventListener('click', handleClick); 
+    square.id = index; 
+}); 
+  
+resetBtnEl.addEventListener('click', init); 
+
+init(); 
